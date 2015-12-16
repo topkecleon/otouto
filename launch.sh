3a1ecb6c59395f5cd946a2aee63b15c6f1944f64
Tiago Danin <tiagojdanin@hotmail.com>
2015-12-16 14:23:59 -0300

    Fix Bug Update

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 if [ "$1" = "install-debian" ]; then
  echo "Preparing..."
  sudo apt-get install lua5.2 lua-sec lua-socket
- sudo apt-get install update
+ sudo apt-get update 
 else
  lua bot.lua
  sleep 5s
