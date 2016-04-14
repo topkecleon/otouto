c74e555831dce4164ab18558ffb6a4355a4d1f06
Brayden <bb010g@gmail.com>
2016-04-14 11:19:01 -0700

    Allow /lua access of the current instance

--- plugins/luarun.lua
+++ plugins/luarun.lua
@@ -19,7 +19,7 @@ function luarun:action(msg)
 		return
 	end
 
-	local output = loadstring(input)()
+	local output = loadstring('function (instance) '..input..' end')()(self)
 	if output == nil then
 		output = 'Done!'
 	elseif type(output) == 'table' then
