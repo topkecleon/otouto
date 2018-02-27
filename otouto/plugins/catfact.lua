850081d7db339c5b37db17c819651f11eb510e71
topkecleon <drew@otou.to>
2018-02-27 13:45:43 -0500

    fix catfact.lua

--- otouto/plugins/catfact.lua
+++ otouto/plugins/catfact.lua
@@ -8,8 +8,8 @@
     This code is licensed under the GNU AGPLv3. See /LICENSE for details.
 ]]--
 
-local JSON = require('dkjson')
-local HTTP = require('socket.http')
+local dkjson = require('dkjson')
+local https = require('ssl.https')
 local utilities = require('otouto.utilities')
 
 local catfact = {}
@@ -18,18 +18,18 @@ function catfact:init()
     catfact.triggers = utilities.triggers(self.info.username, self.config.cmd_pat)
         :t('catfact', true).table
     catfact.command = 'catfact'
-    catfact.doc = 'Returns a cat fact.'
-    catfact.url = 'http://catfacts-api.appspot.com/api/facts'
+    catfact.doc = 'Returns a cat fact from catfact.ninja.'
+    catfact.url = 'https://catfact.ninja/fact'
 end
 
 function catfact:action(msg)
-    local jstr, code = HTTP.request(catfact.url)
+    local jstr, code = https.request(catfact.url)
     if code ~= 200 then
         utilities.send_reply(msg, self.config.errors.connection)
         return
     end
-    local data = JSON.decode(jstr)
-    local output = '*Cat Fact*\n_' .. data.facts[1] .. '_'
+    local data = dkjson.decode(jstr)
+    local output = '*Cat Fact*\n_' .. data.fact .. '_'
     utilities.send_message(msg.chat.id, output, true, nil, true)
 end
 
