137386597282eb869ed16145f7871f51293aac90
topkecleon <drew@otou.to>
2018-03-30 03:10:44 -0400

    groupinfo now stores chat tables in db.groupdata.info

--- otouto/plugins/admin/groupinfo.lua
+++ otouto/plugins/admin/groupinfo.lua
@@ -5,27 +5,37 @@ local autils = require('otouto.autils')
 
 local P = {}
 
-function P:init(_bot)
+function P:init(bot)
     self.triggers = {''}
     self.administration = true
     self.privilege = 2
+    bot.database.groupdata.info = bot.database.groupdata.info or {}
 end
 
-function P:action(bot, msg, group)
-    local admin = group.data.admin
-    admin.username = msg.chat.username
-    -- Log when the chat title has been changed.
-    if msg.chat.title ~= admin.name then
-        autils.log(bot, {
-            chat_id = msg.chat.id,
-            action = 'Title changed',
-            reason = msg.chat.title,
-            -- If this message isn't the message that changed it (the bot didn't
-            -- notice the name change), the source will be unknown.
-            source_id = msg.new_chat_title and msg.from.id or nil
-        })
-        admin.name = msg.chat.title
+function P:action(bot, msg, group, _user)
+    local info = group.data.info
+
+    if not info then
+        group.data.info = msg.chat
+
+    else
+        if info.title ~= msg.chat.title then
+            autils.log(bot, {
+                chat_id = msg.chat.id,
+                action = 'Title changed',
+                reason = msg.chat.title,
+                -- If this message isn't the message that changed it (the bot
+                -- didn't notice the name change), the source will be unknown.
+                source_id = msg.new_chat_title and msg.from.id or nil
+            })
+            info.title = msg.chat.title
+        end
+
+        if info.username ~= msg.chat.username then
+            info.username = msg.chat.username
+        end
     end
+
     return true
 end
 
