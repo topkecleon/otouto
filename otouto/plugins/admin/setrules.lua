720089d40a5ffcb641001d74e3a19f81aac060c1
topkecleon <drew@otou.to>
2018-05-14 13:52:04 -0400

    another setrules change

--- otouto/plugins/admin/setrules.lua
+++ otouto/plugins/admin/setrules.lua
@@ -98,7 +98,7 @@ P.subcommands = {
         elseif not idx then -- /setrules
             admin.rules = new_rules
             local output = '<b>Rules for ' .. utilities.html_escape(
-                utilities.format_name(group.data.info)) .. ':</b>'
+                group.data.info.title) .. ':</b>'
             for i, rule in ipairs(admin.rules) do
                 output = output .. '\n<b>' .. i .. '.</b> ' .. rule
             end
