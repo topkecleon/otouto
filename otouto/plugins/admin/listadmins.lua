513d22aa90c85dbbe73a6308988a326bb9412fe9
Drew <drew@otou.to>
2019-04-16 12:46:25 -0400

    listadmins: remove special entry for bot owner

--- otouto/plugins/admin/listadmins.lua
+++ otouto/plugins/admin/listadmins.lua
@@ -19,8 +19,6 @@ end
 function P:action(bot, msg, _group, _user)
     local admin_list =
         utilities.list_names(bot, bot.database.userdata.administrator)
-    table.insert(admin_list, 1,
-        utilities.lookup_name(bot, bot.config.admin) .. ' ★')
     table.insert(admin_list, 1, '<b>Global administrators:</b>')
     utilities.send_reply(msg, table.concat(admin_list, '\n• '), 'html')
 end
