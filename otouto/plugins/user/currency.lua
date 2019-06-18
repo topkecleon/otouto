d01efb6634da718cca08e80796f0507a38d385ac
Drew <drew@otou.to>
2019-06-18 10:40:31 -0400

    user.currency: Improve reliability and help message.

--- otouto/plugins/user/currency.lua
+++ otouto/plugins/user/currency.lua
@@ -2,6 +2,7 @@ local https = require('ssl.https')
 local json = require('dkjson')
 
 local utilities = require('otouto.utilities')
+local anise = require('extern.anise')
 
 local p = {}
 
@@ -16,20 +17,29 @@ Alias: ]] .. bot.config.cmd_pat .. 'currency'
     self.url = "https://api.exchangeratesapi.io/latest"
     self.out_str = '%s %s = %s %s\nRate: %s\n%s\n<a href="https://exchangeratesapi.io/">Exchange Rates API</a>'
     self.unsupported_str = 'Unsupported currency: '
+    bot.database[self.name] = bot.database[self.name] or {}
+    self.db = bot.database[self.name]
     self:later(bot)
 end
 
 function p:get_rate(from, to)
-    if from == self.latest.base then
-        return self.latest.rates[to]
-    elseif to == self.latest.base then
-        return 1 / self.latest.rates[from] 
+    if from == self.db.base then
+        return self.db.rates[to]
+    elseif to == self.db.base then
+        return 1 / self.db.rates[from] 
     else
-        return (1 / self.latest.rates[from]) * self.latest.rates[to]
+        return (1 / self.db.rates[from]) * self.db.rates[to]
     end
 end
 
 function p:action(bot, msg)
+    if not next(self.db) then
+        self:update(bot)
+        if not next(self.db) then
+            error('No conversion rates available.')
+        end
+    end
+
     local output
     local input = msg.text:upper()
     local from_cur, to_cur = input:match('(%a%a%a) TO (%a%a%a)')
@@ -37,9 +47,9 @@ function p:action(bot, msg)
         local from_val = tonumber(
             input:match('([%d%.]+) ' .. from_cur .. ' TO ' .. to_cur)
         ) or 1
-        if not (self.latest.rates[from_cur] or self.latest.base == from_cur) then
+        if not (self.db.rates[from_cur] or self.db.base == from_cur) then
             output = self.unsupported_str .. from_cur
-        elseif not (self.latest.rates[to_cur] or self.latest.base == to_cur) then
+        elseif not (self.db.rates[to_cur] or self.db.base == to_cur) then
             output = self.unsupported_str .. to_cur
         else
             local rate = self:get_rate(from_cur, to_cur)
@@ -50,24 +60,29 @@ function p:action(bot, msg)
                 string.format("%.2f", from_val * rate),
                 to_cur,
                 string.format("%.4f", rate),
-                self.latest.date
+                self.db.date
             )
         end
     else
-        output = self.doc
+        output = self.doc .. 
+            '\n\nThe following currencies can be converted:\n<code>' ..
+            table.concat(anise.keys(self.db.rates), ' ') .. '</code>'
     end
     utilities.send_reply(msg, output, 'html')
 end
 
  -- Update the rates every hour. The API updates every hour.
 function p:later(bot)
-    local response, code = https.request(self.url)
-    if code == 200 then
-        self.latest = json.decode(response)
-    else
-        error('Unable to connect to exchangeratesapi.io')
-    end
+    self:update(bot)
     bot:do_later(self.name, os.time() + (60 * 60))
+end
+
+function p:update(bot)
+    local jstr, code = https.request(self.url)
+    if code == 200 then
+        bot.database[self.name] = json.decode(jstr)
+        self.db = bot.database[self.name]
+    end
 end
 
 return p
